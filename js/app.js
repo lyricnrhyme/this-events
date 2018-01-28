59af3017e544df888b148d6ad91cda57ac907067
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-01-27 23:53:07 -1000

    Completed Exercise 1

--- js/app.js
+++ js/app.js
@@ -4,7 +4,22 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var navElem = document.getElementsByClassName("navi");
+console.log(navElem);
+for (var i=0; i<navElem.length; i++) {
+	// console.log(navElem[i]);
+	navElem[i].addEventListener("click", openNav);
+}
 
+function openNav() {
+	// console.log("openNav test");
+	var innerNav = this.querySelector(".inner");
+	if(innerNav.style.display === "none") {
+		innerNav.style.display = "block";
+	}else{
+		innerNav.style.display = "none";
+	}
+}
 
 
 
