ad3825db0a0602c66b3400fc39693f8506bbf293
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-01-28 00:00:23 -1000

    Completed Exercise 2

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
 var navElem = document.getElementsByClassName("navi");
-console.log(navElem);
+// console.log(navElem);
 for (var i=0; i<navElem.length; i++) {
 	// console.log(navElem[i]);
 	navElem[i].addEventListener("click", openNav);
@@ -24,3 +24,18 @@ function openNav() {
 
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+
+var menuElem = document.getElementsByClassName("name");
+for (var i=0; i<menuElem.length; i++) {
+	menuElem[i].addEventListener("click", revealMenu);
+}
+
+function revealMenu() {
+	console.log("reveal menu");
+	var menuRevealed = this.querySelector(".menu");
+	if (menuRevealed.style.display === "none") {
+		menuRevealed.style.display = "block";
+	}else{
+		menuRevealed.style.display = "none";
+	}
+}
